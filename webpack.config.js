4fc11e28fc2f9d4fcda87da1133b41261a2e808f
Rodrigo <rodrigocordoba2296@gmail.com>
2022-12-17 15:09:08 -0300

    primer subida

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  }
+}
